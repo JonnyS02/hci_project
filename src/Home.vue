725358474ee9cd0781e05205ea18a52733d9b94b
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-18 22:34:50 +0100

    home style

--- src/Home.vue
+++ src/Home.vue
@@ -1,26 +1,46 @@
+<!-- Home.vue -->
 <template>
-  <Navbar />
-  <div class="main_content">
-    <h1>Home</h1>
+  <div>
+    <Navbar />
+    <div class="main_content">
+      <h1>Welcome back Jonny</h1>
+      <div class="card-container">
+        <CardItem title="Card 1" link="/card1" />
+        <CardItem title="Card 2" link="/card2" />
+        <CardItem title="Card 3" link="/card3" />
+        <CardItem title="Card 4" link="/card4" />
+        <CardItem title="Card 5" link="/card5" />
+        <CardItem title="Card 6" link="/card6" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Links from './components/Links.vue';
 import Navbar from './components/navbar.vue';
-
+import CardItem from './components/CardItem.vue';
 
 export default {
   components: {
-    Links,
     Navbar,
+    CardItem,
   },
-}
+};
 </script>
 
 <style>
-.main_content{
+.main_content {
   padding: 10px;
   margin-left: 15px;
+}
+
+.card-container {
+  padding-top: 12%;
+  width: 100%;
+  padding-left: 20%;
+  padding-right: 20%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; /* Zentriere horizontal */
 }
 </style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,37 @@
+<!-- CardItem.vue -->
+<template>
+    <div class="card-item">
+      <a :href="link" class="card-link">
+        <h2>{{ title }}</h2>
+      </a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: String,
+      link: String,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .card-item {
+    width: 300px;
+    height: 100px;
+    background-color: #e0e0e0;
+    margin: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+  }
+  
+  .card-link {
+    text-decoration: none;
+    color: #333;
+    text-align: center;
+  }
+  </style>
+  
