407c0136e6c419ac74de2b886c141fb5d08653a5
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-19 00:03:07 +0100

    home animation

--- src/Home.vue
+++ src/Home.vue
@@ -5,12 +5,7 @@
     <div class="main_content">
       <h1>Welcome back <span :style="{ color: '#e8672c' }">{{ displayUserName }}</span></h1>
       <div class="card-container">
-        <CardItem title="Card 1" link="/courseprof" />
-        <CardItem title="Card 2" link="/card2" />
-        <CardItem title="Card 3" link="/card3" />
-        <CardItem title="Card 4" link="/card4" />
-        <CardItem title="Card 5" link="/card5" />
-        <CardItem title="Card 6" link="/card6" />
+        <CardItem v-for="(card, index) in cardItems" :key="index" :title="card.title" :link="card.link" :delay="((index)/50)+0.01" />
       </div>
     </div>
   </div>
