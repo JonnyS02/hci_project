b503f017503fb5389fd667722fb79b4f230ed0f2
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-18 23:57:56 +0100

    Home animation

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,37 +1,62 @@
 <!-- CardItem.vue -->
 <template>
-    <div class="card-item">
-      <a :href="link" class="card-link">
-        <h2>{{ title }}</h2>
-      </a>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      title: String,
-      link: String,
+  <div class="card-item" :style="{ 'transition-delay': `${delay}s`, 'background-color': backgroundColor }">
+    <a :href="link" class="card-link">
+      <h2 :style="{ color: textColor }">{{ title }}</h2>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    link: String,
+    delay: Number,
+  },
+  data() {
+    return {
+      textColor: 'rgba(18, 18, 18, 0)', // Start with transparent color
+      backgroundColor: 'rgba(224, 224, 224, 0)', // Start with transparent background-color
+    };
+  },
+  methods: {
+    fadeIn() {
+      setTimeout(() => {
+        let opacity = 0;
+        const interval = setInterval(() => {
+          opacity += 0.1;
+          this.textColor = `rgba(18, 18, 18, ${opacity})`;
+          this.backgroundColor = `rgba(224, 224, 224, ${opacity})`;
+
+          if (opacity >= 1) {
+            clearInterval(interval);
+          }
+        }, 1); // Change the interval to make it faster
+      }, this.delay * 10000);
     },
-  };
-  </script>
-  
-  <style scoped>
-  .card-item {
-    width: 300px;
-    height: 100px;
-    background-color: #e0e0e0;
-    margin: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 5px;
-  }
-  
-  .card-link {
-    text-decoration: none;
-    color: #333;
-    text-align: center;
-  }
-  </style>
-  
+  },
+  mounted() {
+    this.fadeIn();
+  },
+};
+</script>
+
+<style scoped>
+.card-item {
+  width: 300px;
+  height: 100px;
+  margin: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out; /* Double the speed */
+}
+
+.card-link {
+  text-decoration: none;
+  color: hsl(0, 0%, 20%);
+  text-align: center;
+}
+</style>
