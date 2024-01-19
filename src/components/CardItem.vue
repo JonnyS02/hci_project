a89107e3f69540dd65fa356be1f24b367a2d1ef0
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-19 01:18:12 +0100

    style fix

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,6 +1,5 @@
-<!-- CardItem.vue -->
 <template>
-  <div class="card-item" :style="{ 'transition-delay': `${delay}s`, 'background-color': backgroundColor }">
+  <div class="card-item" :style="{ 'transition-delay': `${delay}s`, 'background-color': backgroundColor, 'border-color': borderColor }">
     <a :href="link" class="card-link">
       <h2 :style="{ color: textColor }">{{ title }}</h2>
     </a>
@@ -18,6 +17,7 @@ export default {
     return {
       textColor: 'rgba(18, 18, 18, 0)', 
       backgroundColor: 'rgba(200, 200, 200, 0)',
+      borderColor: 'rgba(232, 103, 44, 0)', // Initialer Wert für die Borderfarbe
     };
   },
   methods: {
@@ -28,6 +28,7 @@ export default {
           opacity += 0.1;
           this.textColor = `rgba(18, 18, 18, ${opacity})`;
           this.backgroundColor = `rgba(200, 200, 200, ${opacity})`;
+          this.borderColor = `rgba(232, 103, 44, ${opacity})`;
 
           if (opacity >= 1) {
             clearInterval(interval);
@@ -51,7 +52,8 @@ export default {
   align-items: center;
   justify-content: center;
   border-radius: 5px;
-  transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out;
+  transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out, border-color 0.5s ease-in-out; /* Füge die Border-Transition hinzu */
+  border: 1px solid rgba(232, 103, 44, 0); /* Initialer Wert für die Border */
 }
 
 a:hover {
@@ -59,14 +61,6 @@ a:hover {
 }
 
 .card-item:hover {
-  box-shadow: 0 0 11px rgba(21, 45, 58, 255);
   background: linear-gradient(to right, rgba(231, 101, 44, 255) 0%, rgba(231, 101, 44, 255) 100%);
-  color: rgb(255, 255, 255);
-}
-
-.card-link {
-  text-decoration: none;
-  color: hsl(0, 0%, 20%);
-  text-align: center;
 }
 </style>
