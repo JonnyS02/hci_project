81e7ee2bc44034d349f2b43760ee8075e77f5b49
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-29 16:33:30 +0100

    raumnummer

--- src/Schedule.vue
+++ src/Schedule.vue
@@ -4,51 +4,43 @@
 
         <table>
             <tr>
-                <th>Zeit</th>
-                <th>Montag</th>
-                <th>Dienstag</th>
-                <th>Mittwoch</th>
-                <th>Donnerstag</th>
-                <th>Freitag</th>
-            </tr>
-            <tr>
                 <td class="time">8:15 - 9:45</td>
-                <td>Exobiologie</td>
-                <td>Astrophysik</td>
+                <td>Exobiologie<br><span class="raum">F Gebäude 59</span></td>
+                <td>Astrophysik<br><span class="raum">C Gebäude 17</span></td>
                 <td></td>
-                <td>Planetenwissenschaften</td>
+                <td>Planetenwissenschaften<br><span class="raum">E Gebäude 56</span></td>
                 <td></td>
             </tr>
             <tr>
                 <td class="time">10:15 - 11:45</td>
                 <td></td>
-                <td>Astronomie</td>
-                <td>Raumfahrttechnik</td>
+                <td>Astronomie<br><span class="raum">A Gebäude 23</span></td>
+                <td>Raumfahrttechnik<br><span class="raum">D Gebäude 35</span></td>
                 <td></td>
-                <td>Exobiologie</td>
+                <td>Exobiologie<br><span class="raum">B Gebäude 42</span></td>
             </tr>
             <tr>
                 <td class="time">12:15 - 13:45</td>
-                <td>Raumfahrttechnik</td>
+                <td>Raumfahrttechnik<br><span class="raum">C Gebäude 64</span></td>
                 <td></td>
                 <td></td>
-                <td>Astrophysik</td>
+                <td>Astrophysik<br><span class="raum">A Gebäude 47</span></td>
                 <td></td>
             </tr>
             <tr>
                 <td class="time">14:15 - 15:45</td>
                 <td></td>
                 <td></td>
-                <td>Exobiologie</td>
+                <td>Exobiologie<br><span class="raum">C Gebäude 29</span></td>
                 <td></td>
-                <td>Astronomie</td>
+                <td>Astronomie<br><span class="raum">D Gebäude 12</span></td>
             </tr>
             <tr>
                 <td class="time">16:15 - 17:45</td>
-                <td>Astrophysik</td>
+                <td>Astrophysik<br><span class="raum">F Gebäude 19</span></td>
                 <td></td>
-                <td>Planetenwissenschaften</td>
-                <td>Raumfahrttechnik</td>
+                <td>Planetenwissenschaften<br><span class="raum">A Gebäude 85</span></td>
+                <td>Raumfahrttechnik<br><span class="raum">F Gebäude 81</span></td>
                 <td></td>
             </tr>
             <tr>
@@ -78,10 +70,15 @@ table {
     width: 100%;
 }
 
-th,td {
+th,
+td {
     text-align: left;
 
     border: 1px solid #dddddd70;
     padding: 15px;
+}
+
+.raum {
+    font-weight: 100;
 }
 </style>
